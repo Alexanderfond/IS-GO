6ebe2b9127463a504952073d4226d46de5f72bc1
Alexanderfond <91311527+Alexanderfond@users.noreply.github.com>
2022-06-16 13:35:10 +0300

    Update main.go

--- main.go
+++ main.go
@@ -4,32 +4,32 @@ import "fmt"
 
 func main() {
 	var (
-		j = 5.99
-		g = 7
-		bt = 23
+		applePrice float64 = 5.99
+		pearPrice float64 = 7
+		Budget float64 = 23
 	)
-	v1 := (9 * j + 8 * float64(g))
-	v2 := bt / g
-	var v3 = float64(bt) / j
+	v1 := 9 * applePrice + 8 * pearPrice
+	v2 := Budget / pearPrice
+	v3 := Budget / applePrice
 
-	s := 2 * j + 2 * float64(g)
+	s := 2 * applePrice + 2 * pearPrice
 	
 	fmt.Println(
-		"costApple", j, 
-		"costPear", g,
-		"Budget", bt)
+		"costApple:", applePrice,
+		"costPear:", pearPrice,
+		"Budget:", Budget)
 
 	fmt.Println(
 		"1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?", v1,
 		"\n2. Скільки груш ми можемо купити?", v2,
 		"\n3. Скільки яблук ми можемо купити?", v3)
 
-	fmt.Println("4. Чи ми можемо купити 2 груші та 2 яблука?")
+	fmt.Print("4. Чи ми можемо купити 2 груші та 2 яблука?")
 
-	if float64(bt) >= s {
-		fmt.Println("можемо")
+	if Budget >= s {
+		fmt.Print(" - можемо")
 	} else { 
-		fmt.Println("не можемо") 
+		fmt.Print(" - не можемо") 
 	}
 	
 }
